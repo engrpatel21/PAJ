f4d8a3a0b070ba942f36be000c867a1c819163d8
pratik <engrpatel21@gmail.com>
2020-09-16 15:09:26 -0400

    tasks schema complete

--- models/project.jsx
+++ models/project.jsx
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const taskSchema = new Schema({
-    
+const taskSchema = new Schema({ 
+    user: { type: Schema.Types.ObjectId, ref: 'User'},
+    name: String,
+    content: String,
+    taskStatus:{type: String, enum: ['In Progress', 'Completed', 'Backlog']}
 })
 
 const projectSchema = new Schema({
