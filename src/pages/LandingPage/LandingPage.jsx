045dce5ac6927fe63fcfc49679f998f268e1dd56
A.I. Kasem <ak89mkii@gmail.com>
2020-09-17 17:56:52 -0400

    Revised NavBar with semantic ui

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,45 +1,14 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react'
+import { Segment, Image } from 'semantic-ui-react'
 import logo from './logo.png';
 
-const LandingPage = (props) => {
-    return (
-        <>
-        <h1>Landing Page</h1>
-        <div class="logo">
-        <Grid verticalAlign='middle' columns={2} centered>
-            <Grid.Column>
-                <Image src = {logo} />
-            </Grid.Column>
-        </Grid>
-        </div>
-        <div>
-            Welcome to PA-J (Pronounced page), a practical and feature-rich project management platform.
-        </div>
-        <br></br>
-        <div>
-        <a href="/projectboard">To Project Board (Temporary)</a>
-        </div>
-        <br></br>
-        <div>
-        <a href="/projectdetails">To Project Details (Temporary)</a>
-        </div>
-
-        {/* <h1>To Project Board (Temp)</h1>
-            {this.props.projectBoard.map((projectboard) =>
-            <div key={projectboard._id}>
-                <Link
-                to={{
-                    pathname: "/projectboard",
-                    state: { projectboard }
-                    }}
-                >
-                {projectboard.name}
-                </Link><br></br>
-                </div>
-            )} */}
-        </>
+const LandingPage = () => (
+    <Segment>
+        <Image src={logo} size='xxlarge' centered />
+        <p>
+        Welcome to PA-J (Pronounced page), a practical and feature-rich project management platform.
+        </p>
+    </Segment>
     )
-}
 
 export default LandingPage;
