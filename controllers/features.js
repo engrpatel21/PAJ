4388a0fcdd79e750e06b4b3c56fef347a845d3f8
pratik <engrpatel21@gmail.com>
2020-09-17 08:18:48 -0400

    can show 1 feature at a time

--- controllers/features.js
+++ controllers/features.js
@@ -1,11 +1,20 @@
 const Project = require('../models/project')
 
 module.exports = {
+    index,
     createFeature,
-    showFeature
+    showFeature,
 }
 
-function showFeature(req,res){
+function showFeature(req, res){
+    Project.findById(req.params.projectId)
+    .then(project => {
+        const idx = project.features.findIndex(feature => feature._id.equals(req.params.featureId))
+        res.json(project.features[idx])
+    })
+}
+
+function index(req,res){
     Project.findById(req.params.projectId)
     .then(project =>
         res.json(project.features)
--- routes/features.js
+++ routes/features.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const featureCtrl = require('../controllers/features')
 
-
-router.get('/:projectId/features', featureCtrl.showFeature)
+router.get('/:projectId/features', featureCtrl.index)
+router.get('/:projectId/features/:featureId', featureCtrl.showFeature)
 router.post('/:projectId/features', featureCtrl.createFeature)
 
 
