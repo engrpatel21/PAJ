09c539c35c3b92b59199d7c4706fe8463498e838
pratik <engrpatel21@gmail.com>
2020-09-17 17:59:57 -0400

    index for messages

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,7 +1,16 @@
 const User = require('../models/user')
 
 module.exports ={
+    index,
     createMessage
+}
+
+function index(req, res){
+    User.findById(req.params.userId)
+    .populate('createdBy')
+    .then(user => 
+        res.json(user.messages)
+        )
 }
 
 function createMessage(req, res){
--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const messageCtrl = require('../controllers/messages')
 
+router.get('/:userId/messages', messageCtrl.index)
 router.post('/:userId/messages', messageCtrl.createMessage)
 
 module.exports = router
