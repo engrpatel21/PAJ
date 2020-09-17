61dd60f9cd50db2dab5da7ea93fb289d43a200a1
pratik <engrpatel21@gmail.com>
2020-09-17 18:04:47 -0400

    delete for messages

--- controllers/messages.js
+++ controllers/messages.js
@@ -2,7 +2,19 @@ const User = require('../models/user')
 
 module.exports ={
     index,
-    createMessage
+    createMessage,
+    deleteMessage
+}
+
+function deleteMessage(req, res){
+    User.findById(req.params.userId)
+    .then(user =>{
+        const idx = user.messages.findIndex(message => message._id.equals(req.params.messageId))
+        user.messages.splice(idx,1)
+        user.save().then(user =>
+            res.json(user.messages)
+            )
+    })
 }
 
 function index(req, res){
--- routes/messages.js
+++ routes/messages.js
@@ -3,5 +3,6 @@ const messageCtrl = require('../controllers/messages')
 
 router.get('/:userId/messages', messageCtrl.index)
 router.post('/:userId/messages', messageCtrl.createMessage)
+router.delete('/:userId/messages/:messageId', messageCtrl.deleteMessage)
 
 module.exports = router
