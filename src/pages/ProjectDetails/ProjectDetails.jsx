313a14aded1fb5f1c3709d122434f26167ae8086
A.I. Kasem <ak89mkii@gmail.com>
2020-09-17 20:20:03 -0400

    Separated dettails page into 3 sections

--- src/pages/ProjectDetails/ProjectDetails.jsx
+++ src/pages/ProjectDetails/ProjectDetails.jsx
@@ -1,35 +1,34 @@
 import React from 'react';
 import { Link } from "react-router-dom"
+import { Message, Icon, Divider, Segment, Image } from 'semantic-ui-react'
 import "./ProjectDetails.css";
 
-const ProjectDetails = (props) => {
-    return (
-        <div class="textDiv">
-            <h1>Project Details Page</h1>
-            <div>
-                <h2>Project Information</h2>
-            </div>
-            <div>
-                Semantic UI grid for project trackins columns.
-            </div>
-            <br></br>
-            <div>
-                <h2>Links to Feature Sets</h2>
-            </div>
-            <div>
-                Semantic UI grid for project trackins columns.
-            </div>
-            <br></br>
-            <div>
-                <h2>Add Comments Here</h2>
-            </div>
-            <div>
-                Semantic UI grid for project trackins columns.
-            </div>
-            <br></br>
-            <a href="/">RETURN</a>
-        </div>
-    )
-}
+const ProjectDetails = () => (
+    <>
+    <h1>Project Details Page</h1>
+    <Message>
+        
+        <Message.Header>Project Information</Message.Header>
+        <p>
+            We updated our privacy policy here to better service our customers. We
+            recommend reviewing the changes.
+        </p>
+    </Message>
+    <Message>
+    <Message.Header>Links to Feature Sets</Message.Header>
+    <p>
+        We updated our privacy policy here to better service our customers. We
+        recommend reviewing the changes.
+    </p>
+    </Message>
+    <Message>
+    <Message.Header>Links to Feature Sets</Message.Header>
+    <p>
+        We updated our privacy policy here to better service our customers. We
+        recommend reviewing the changes.
+    </p>
+    </Message>
+    </>
+)
 
 export default ProjectDetails;
