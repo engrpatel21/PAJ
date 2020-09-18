8dc55d7ce298c8988286412acdcbb0a8ef9ca79d
A.I. Kasem <ak89mkii@gmail.com>
2020-09-17 23:43:00 -0400

    Basic scaffolding for project baord page

--- src/pages/ProjectBoard/ProjectBoard.jsx
+++ src/pages/ProjectBoard/ProjectBoard.jsx
@@ -34,25 +34,75 @@ const ProjectBoard = () => (
                         </Card.Content>
                     </Card>
                 </Card.Group>
-   
                     </Grid.Column>
                     <Grid.Column>
                 <h1>Completed:</h1>
-                <p>Data</p>
-                <p>Data</p>
-                <p>Data</p>
-                <p>Data</p>
-                <p>Data</p>
-                <p>Data</p>
-                <p>Data</p>
-                <p>Data</p>
+                <Card.Group centered items>
+                    <Card>
+                        <Card.Content>
+                            <Card.Header>Placeholder Project</Card.Header>
+                            <Card.Meta>Head of Project Here</Card.Meta>
+                            <Card.Description>
+                            Specific project objectives will go <strong>IN THIS AREA</strong>
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <div className='ui two buttons'>
+                            <Button basic color='green'>
+                                DETAILS
+                            </Button>
+                            <Button basic color='red'>
+                                DELETE
+                            </Button>
+                            </div>
+                        </Card.Content>
+                    </Card>
+                </Card.Group>
             </Grid.Column>
             <Grid.Column>
                 <h1>Backlog:</h1>
-                <p>Data</p>
-                <p>Data</p>
-                <p>Data</p>
-                <p>Data</p>
+                <Card.Group centered items>
+                    <Card>
+                        <Card.Content>
+                            <Card.Header>Placeholder Project</Card.Header>
+                            <Card.Meta>Head of Project Here</Card.Meta>
+                            <Card.Description>
+                            Specific project objectives will go <strong>IN THIS AREA</strong>
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <div className='ui two buttons'>
+                            <Button basic color='green'>
+                                DETAILS
+                            </Button>
+                            <Button basic color='red'>
+                                DELETE
+                            </Button>
+                            </div>
+                        </Card.Content>
+                    </Card>
+                </Card.Group>
+                <Card.Group centered items>
+                    <Card>
+                        <Card.Content>
+                            <Card.Header>Placeholder Project</Card.Header>
+                            <Card.Meta>Head of Project Here</Card.Meta>
+                            <Card.Description>
+                            Specific project objectives will go <strong>IN THIS AREA</strong>
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <div className='ui two buttons'>
+                            <Button basic color='green'>
+                                DETAILS
+                            </Button>
+                            <Button basic color='red'>
+                                DELETE
+                            </Button>
+                            </div>
+                        </Card.Content>
+                    </Card>
+                </Card.Group>
             </Grid.Column>
         </Grid.Row>
     </Grid>
