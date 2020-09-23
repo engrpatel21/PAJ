d51fce18e3c4032236d97f74a8a2f52dfe0f8fb9
A.I. Kasem <ak89mkii@gmail.com>
2020-09-22 21:49:10 -0400

    Message board, passing users and separate message component

--- src/pages/MessagePage/MessagePage.jsx
+++ src/pages/MessagePage/MessagePage.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
+import * as userApi from "../../services/userService"
 import { Button, Comment, Form, Header, Divider, Grid} from 'semantic-ui-react'
-import Ryu from './01.png'
-import Ken from './02.png'
-import Bison from './03.png'
-import Guile from './04.png'
+import MessageComponent from '../../components/MessageComponent/MessageComponent'
 
-const MessagePage = () => (
+class MessagePage extends Component {
+  state = { 
+      users: [],
+
+   }
+
+  async componentDidMount(){
+      const users = await userApi.getAllUsers()
+      this.setState({users})
+  }
+
+  render() { 
+    const {users} = this.state
+    return ( 
+    <>
 
   <Grid centered columns={1}>
   <Comment.Group>
@@ -16,92 +28,9 @@ const MessagePage = () => (
       Messages
     </Header>
 
-    <Comment>
-      <Comment.Avatar src={Ryu} />
-      <Comment.Content>
-        <Comment.Author as='a'>Ryu</Comment.Author>
-        <Comment.Metadata>
-          <div>Today at 5:42PM</div>
-        </Comment.Metadata>
-        <Comment.Text>Hadouken!</Comment.Text>
-        <Comment.Actions>
-          <Comment.Action>Reply</Comment.Action>
-        </Comment.Actions>
-      </Comment.Content>
-    </Comment>
-
-    <Comment>
-      <Comment.Avatar src={Ken} />
-      <Comment.Content>
-        <Comment.Author as='a'>Ken</Comment.Author>
-        <Comment.Metadata>
-          <div>Yesterday at 12:30AM</div>
-        </Comment.Metadata>
-        <Comment.Text>
-          <p>Shoruken!</p>
-        </Comment.Text>
-        <Comment.Actions>
-          <Comment.Action>Reply</Comment.Action>
-        </Comment.Actions>
-      </Comment.Content>
-      <Comment.Group>
-        <Comment>
-          <Comment.Avatar src={Bison} />
-          <Comment.Content>
-            <Comment.Author as='a'>M. Bison</Comment.Author>
-            <Comment.Metadata>
-              <div>Just now</div>
-            </Comment.Metadata>
-            <Comment.Text>Psycho Breaker :)</Comment.Text>
-            <Comment.Actions>
-              <Comment.Action>Reply</Comment.Action>
-            </Comment.Actions>
-          </Comment.Content>
-        </Comment>
-      </Comment.Group>
-    </Comment>
-
-    <Comment>
-      <Comment.Avatar src={Guile} />
-      <Comment.Content>
-        <Comment.Author as='a'>Guile</Comment.Author>
-        <Comment.Metadata>
-          <div>5 days ago</div>
-        </Comment.Metadata>
-        <Comment.Text>Sonic Boom!</Comment.Text>
-        <Comment.Actions>
-          <Comment.Action>Reply</Comment.Action>
-        </Comment.Actions>
-      </Comment.Content>
-    </Comment>
-
-    <Comment>
-      <Comment.Avatar src='https://vignette.wikia.nocookie.net/starfox/images/7/77/Foxhead.gif/revision/latest/top-crop/width/220/height/220?cb=20081028023846' />
-      <Comment.Content>
-        <Comment.Author as='a'>Fox</Comment.Author>
-        <Comment.Metadata>
-          <div>5 days ago</div>
-        </Comment.Metadata>
-        <Comment.Text>Who are these guys?</Comment.Text>
-        <Comment.Actions>
-          <Comment.Action>Reply</Comment.Action>
-        </Comment.Actions>
-      </Comment.Content>
-    </Comment>
-
-    <Comment>
-      <Comment.Avatar src='https://vignette.wikia.nocookie.net/starfox/images/9/9e/Falcohead.gif/revision/latest/top-crop/width/220/height/220?cb=20081028023846' />
-      <Comment.Content>
-        <Comment.Author as='a'>Falco</Comment.Author>
-        <Comment.Metadata>
-          <div>5 days ago</div>
-        </Comment.Metadata>
-        <Comment.Text>Different franchise...</Comment.Text>
-        <Comment.Actions>
-          <Comment.Action>Reply</Comment.Action>
-        </Comment.Actions>
-      </Comment.Content>
-    </Comment>
+    {users.map(user =>
+    <MessageComponent key={user._id} user={user} username={user.name} />
+    )}
 
     <Form reply>
       <Form.TextArea />
@@ -109,6 +38,10 @@ const MessagePage = () => (
     </Form>
   </Comment.Group>
   </Grid>
-)
+
+  </>
+         );
+    }
+}
 
 export default MessagePage
